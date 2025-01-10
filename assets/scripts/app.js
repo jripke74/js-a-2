612aca4d57757ec8505462afa2d4cc51d39216cd
Jeff Ripke <jripke74@gmail.com>
2025-01-10 10:28:37 -0800

    add alert()

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -6,3 +6,16 @@ currentResult = ((currentResult + 10) * 3) / 2 - 1;
 let calculationDescription = `(${defaultResult} + 10) * 3 / 2 - 1`;
 
 outputResult(currentResult, calculationDescription);
+
+
+// assingment
+const userInputB = 2;
+let result = 0;
+
+result = userInputB + 18;
+result = result - 3;
+result = result * 4;
+result = result / 5;
+
+alert(result);
+alert(userInputB);
