70901a3e34aaad4a3691bb0a6c5ed5295c10a9c9
Jeff Ripke <jripke74@gmail.com>
2025-01-11 08:38:06 -0800

    aed greetings

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -3,7 +3,30 @@ let currentResult = defaultResult;
 
 function add() {
   currentResult = currentResult + userInput.value;
-  outputResult(currentResult, '');
+  outputResult(currentResult, "");
 }
 
 addBtn.addEventListener("click", add);
+
+// assingment 2
+const task3Element = document.getElementById("task-3");
+
+function greet() {
+  alert("hello");
+}
+
+function greetUser(name) {
+  alert(name);
+}
+
+function fullName(first, last, middle) {
+  return `${first} ${last}, ${middle}`;
+}
+
+greet();
+greetUser("Jeff");
+
+task3Element.addEventListener("click", greet);
+
+const combinedName = fullName('Jeff', 'Ripke', 'Allen')
+alert(combinedName);
