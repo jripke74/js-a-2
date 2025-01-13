57911220c8893fc80d8aeb10640e2b6a40c44152
Jeff Ripke <jripke74@gmail.com>
2025-01-13 11:41:19 -0800

    add parseInt()

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -2,31 +2,8 @@ const defaultResult = 0;
 let currentResult = defaultResult;
 
 function add() {
-  currentResult = currentResult + userInput.value;
+  currentResult = currentResult + parseInt(userInput.value);
   outputResult(currentResult, "");
 }
 
 addBtn.addEventListener("click", add);
-
-// assingment 2
-const task3Element = document.getElementById("task-3");
-
-function greet() {
-  alert("hello");
-}
-
-function greetUser(name) {
-  alert(name);
-}
-
-function fullName(first, last, middle) {
-  return `${first} ${last}, ${middle}`;
-}
-
-greet();
-greetUser("Jeff");
-
-task3Element.addEventListener("click", greet);
-
-const combinedName = fullName('Jeff', 'Ripke', 'Allen')
-alert(combinedName);
